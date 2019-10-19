f35cf0f9e23969f97dfec037cad4116f822869e0
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-19 16:17:42 +0100

    created IndexScreen

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const IndexScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
