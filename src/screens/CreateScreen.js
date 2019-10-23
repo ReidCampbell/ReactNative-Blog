4b36fd83b7bca342f5a1ae5448ed421ee2dd21bf
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-23 16:11:55 +0100

    added show screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+import { Context } from "../context/BlogContext";
+
+const CreateScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
